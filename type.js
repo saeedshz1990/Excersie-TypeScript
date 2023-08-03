3c3ec5bbd5bed63ca80c1f7281a5c0a4e061790b
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2023-08-03 09:27:25 +0330

    continue declar variable in typescript second Part

--- type.js
+++ type.js
@@ -6,12 +6,29 @@ var e;
 var f = 85.5;
 var g = false;
 var h = null;
+var i;
+var j = [1, 2, 'hello'];
+var k = [1, 2, 4];
+var l = [1, 2, 3, 4, 5, 6, 7, 85];
+var m = [false, 1, 2, 3, 'hello'];
+var n = ['hi', 80, true]; //tuple
+//object
+var o = { name: 'Saeed', age: 30, id: 1, city: 'vancouver' };
+console.log();
+j.push('Hi');
+k.push(78);
 console.log(a);
 console.log(typeof (f));
 console.log(Boolean(undefined));
 console.log(Boolean(null));
 //boolean can not + in number
-var z = b + g;
+// let z = b + g;
 //null || undefined != '';
 //null || undefined != false;
 //null || undefined != 0;
+// console.log(8 - 'hello');//NAN
+console.log(2 / 0); //infinity
+console.log(-2 / 0); //infinity
+console.log(-2 / 0); //infinity
+i = 18;
+i = 'hello';
--- type.ts
+++ type.ts
@@ -6,7 +6,19 @@ let e: boolean;
 let f: number = 85.5;
 let g = false;
 let h: null = null;
+let i: any;
+let j = [1, 2, 'hello'];
+let k: number[] = [1, 2, 4];
+let l: Array<number> = [1, 2, 3, 4, 5, 6, 7, 85];
+let m: Array<any> = [false, 1, 2, 3, 'hello'];
+let n: [string, number, boolean] = ['hi', 80, true];//tuple
+//object
+let o = {name: 'Saeed', age: 30, id: 1, city: 'vancouver'};
 
+
+console.log();
+j.push('Hi');
+k.push(78);
 console.log(a);
 console.log(typeof (f));
 console.log(Boolean(undefined));
@@ -14,8 +26,15 @@ console.log(Boolean(null));
 
 
 //boolean can not + in number
-let z = b + g;
+// let z = b + g;
 
 //null || undefined != '';
 //null || undefined != false;
 //null || undefined != 0;
+
+// console.log(8 - 'hello');//NAN
+console.log(2 / 0);//infinity
+console.log(-2 / 0);//infinity
+console.log(-2 / 0);//infinity
+i = 18;
+i = 'hello';
